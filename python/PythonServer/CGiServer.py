647a58217f539233b0ebe556fd334e2b1a9d502f
nagao_hiroki <zerosiseinokae@yahoo.co.jp>
2016-11-08 13:57:00 +0900

    python server を追加

---
+++ python/PythonServer/CGiServer.py
@@ -0,0 +1,6 @@
+﻿# -*- coding: utf-8 -*-
+import CGIHTTPServer
+if __name__ == '__main__':
+    print('あああ')
+    CGIHTTPServer.test();
+
---
+++ python/PythonServer/cgi-bin/top.py
@@ -0,0 +1,6 @@
+﻿# coding:utf-8
+import CGIHTTPServer
+if __name__ == '__main__':
+    txt = open('test.txt', 'w')
+    txt.write('aaa')
+    
